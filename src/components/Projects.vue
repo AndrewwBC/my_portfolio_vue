51a7840a5d7599cdb115c98fb5f7ddb187ed09bf
AndrewwBC <andrew-hot2011@hotmail.com>
2024-04-12 00:55:10 -0300

    add: third route: projects, to vue router

---
+++ src/components/Projects.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "Projects",
+};
+</script>
+
+<template>
+  <h1>Projects</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import IntroVue from "./components/Intro.vue";
 import HelloWorldVue from "./components/HelloWorld.vue";
+import Projects from "./components/Projects.vue";
+
 import { createRouter, createWebHistory } from "vue-router";
 
 import LayoutVue from "./components/Layout.vue";
@@ -10,6 +12,7 @@ import LayoutVue from "./components/Layout.vue";
 const routes = [
   { path: "/", component: IntroVue },
   { path: "/about", component: HelloWorldVue },
+  { path: "/projects", component: Projects },
 ];
 
 const router = createRouter({
